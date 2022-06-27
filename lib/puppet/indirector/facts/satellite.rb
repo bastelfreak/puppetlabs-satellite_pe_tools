04d16372f64ac78d159718e508dfef4a910318ba
Simon Hoenscheid <Simon.Hoenscheid@schufa.de>
2022-06-27 09:29:46 +0200

    implemented feedback

--- lib/puppet/indirector/facts/satellite.rb
+++ lib/puppet/indirector/facts/satellite.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require File.dirname(__FILE__) + '/../../util/satellite.rb'
+require __dir__ + '/../../util/satellite.rb'
 require 'puppet/indirector/facts/puppetdb'
 
 # satellite.rb
